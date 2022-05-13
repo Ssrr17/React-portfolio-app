6433d56cefe0b79c483ce694a2c95cce55e0a2fc
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-05-13 18:12:38 -0400

    nav bar added

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,13 +1,13 @@
-import About from './components/about';
-import './App.css';
-import React  from 'react';;
+import About from "./components/About";
+import Nav from "./components/Nav";
+import "./App.css";
+import React from "react";
 // import logo from './logo.svg';
-
-
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
