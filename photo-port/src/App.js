69fbf36aba7b9c937c50f351f1ee34e96a85bdf3
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-05-13 15:42:51 -0400

    complilation successful

---
+++ photo-port/src/App.js
@@ -0,0 +1,18 @@
+import About from './components/about';
+import './App.css';
+import React  from 'react';;
+// import logo from './logo.svg';
+
+
+
+function App() {
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
